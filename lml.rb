3b83ccd7f369fbb417000a33bc4c9cc1f8d93694
herber523 <herber523@gmail.com>
2016-11-14 16:21:42 +0800

    add apriori

--- lml.rb
+++ lml.rb
@@ -19,7 +19,11 @@ module LML
 
   class NB
     def initialize(data)
-      @data_rows = []
+      @data_rows = load_data(data)
+    end
+
+    def load_data(data)
+      data_rows = []
       data_count = data[0][:data].count
       types = data.map { |n| n[:type] }.uniq
       types.each do |type|
@@ -34,8 +38,9 @@ module LML
           mean << t.inject(:+).to_f / t.count
           variance << 2 * t.var
         end
-        @data_rows << { mean: mean, variance: variance, type: type }
+        data_rows << { mean: mean, variance: variance, type: type }
       end
+      data_rows
     end
 
     def classify(input)
@@ -52,6 +57,71 @@ module LML
 
     def pdf(n, mean, variance)
       (1 / Math.sqrt(3.1415 * variance)) * Math.exp(-((n - mean)**2) / variance)
+    end
+  end
+
+  class APRIORI
+    def initialize(data)
+      @data = data.map{|n| n[:data]}
+    end
+    def find_item
+      set = []
+      @data.each do |transction|
+        transction.each do |item|
+          set << item unless set.include?(item)
+        end
+      end
+      set.sort
+    end
+    def find_support(d, ck, min_support)
+      sscnt = {}
+      d.each do |tid|
+        ck.each do |can|
+          can = [can] unless can.is_a?(Array)
+          if (can - tid).empty?
+            sscnt[can] = 0 if sscnt[can].nil?
+            sscnt[can] += 1
+          end
+        end
+      end
+      items_num = d.count.to_f
+      support_data = {}
+      retlist = []
+      sscnt.keys.each do |key|
+        support = sscnt[key] / items_num
+        retlist << key if support >= min_support
+        support_data[key] = support
+      end
+      [retlist, support_data]
+    end
+    def apriori_gen(l1, k)
+      d = []
+      lc = l1.count
+      (0..lc - 1).each do |i|
+        (i + 1..lc - 1).each do |j|
+          if l1[i].is_a?(Array)
+            d << (l1[i] | l1[j]).sort if l1[i].first(k) == l1[j].first(k)
+          else
+            d << ([l1[i]] | [l1[j]]).sort
+          end
+        end
+      end
+      d.sort
+    end
+    def apriori(min_support)
+      data = @data
+      c1 = find_item
+      l1, support_data = find_support(data, c1, min_support)
+      l = [l1]
+      k = 0
+      while l[k].count > 0
+        ck = apriori_gen(l[k], k)
+        lk, supk = find_support(data, ck, min_support)
+        support_data = support_data.merge(supk)
+        l << lk
+        k += 1
+      end
+      [l, support_data]
     end
   end
 end
